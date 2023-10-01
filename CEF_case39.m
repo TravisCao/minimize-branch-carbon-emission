bb6779462d136b21b2e2c08099e07867522ba8be
Travis Cao <travisyjcao@gmail.com>
2023-10-01 13:17:44 +0800

    T = 24, load changes, no renewable for now

--- CEF_case39.m
+++ CEF_case39.m
@@ -1,5 +1,5 @@
 % function [En, Rg, RL, Rb1, Rl, CE_Gen, CE_Load, CE_Loss, obj] = CEF_case39(Pg)
-function [obj] = CEF_case39(T, mpc, Eg, Pg)
+function [obj] = CEF_case39(T, mpc, Eg, HourlyLoadProfile, Pg)
     %……输入变量 
     %T—时间间隔，mpc—电网参数，Eg—发电机排放强度
     %……输出变量
@@ -10,7 +10,7 @@ function [obj] = CEF_case39(T, mpc, Eg, Pg)
     %% %%%%%%%%%%%%%
     % T = 1;    % 持续时间 h
     % mpc = loadcase('case39');
-    n_bus = size(mpc.bus, 1);
+    % n_bus = size(mpc.bus, 1);
     
     % Gn = length(mpc.gen(:, 1));  %bsn = length(mpc.bus(:,1));   brn = length(mpc.branch(:,1));
     % Eg = zeros(Gn, 1);           %  ECEF intensity   % 发电机碳排放强度
@@ -18,98 +18,112 @@ function [obj] = CEF_case39(T, mpc, Eg, Pg)
     % Eg(300 < mpc.gen(:,2) & mpc.gen(:, 2) <= 600) = 0.500;  
     
     % 单一时间断面
-    mpc.gen(:, 2) = Pg;
+    % mpc.gen(:, 2) = Pg;
+
+    % T = 24;
 
     % 目标最小化区间 [1, 39], [4, 5], [13 14]
     targetBranch_row = [1 4 13];
     targetBranch_col = [39 5 14];
-    sz = [n_bus, n_bus];
+    sz = [39, 39];
     target_ind = sub2ind(sz, targetBranch_row, targetBranch_col);
 
     %% 主程序
     % opt = mpoption('out.all', 1, 'verbose', 0, 'opf.init_from_mpc', 1);
     % results =  runopf(mpc, opt);
-
-    % result
-    [results, success] = runpf(mpc);
+    obj = zeros(T, 1);
+    nGens = 10;
+    for t = 1:T
+        mpc.bus(:, 3) = HourlyLoadProfile(:, t);
+        mpc.gen(:, 2) = Pg((t - 1) * nGens + 1: t * nGens);
+        obj(t) = CEF_hourly(mpc);
+    end
     
-    if success == 0
-        obj = abs(sum(Pg) - sum(mpc.bus(:, 3)));
-        return
-    end
+    obj = mean(obj);
 
-    %% 节点分类  
-    Ejn = results.gen(:,1);  gn = length(Ejn);
-    bsn = length(results.bus(:,1));  brn = length(results.branch(:,1));
-    Ijn = results.bus(:,1);
-    Ijn(Ejn)=[];
-    %% ECEF intensity 
-    PG = zeros(bsn,gn); 
-    for g=1:gn
-        PG(Ejn(g),g) = results.gen(g,2);
-    end
-    Rg = PG*Eg;                % ECEF rate 
-    %% ICEE intensity
-    Br_o = results.branch;
-    Pb1 = zeros(bsn);   %节点注入功率矩阵
-    Pb = zeros(bsn);    %节点流出功率矩阵
-    LoN = find(results.bus(:,3)>0);   ln = length(LoN);   
-    Lcp = results.bus(LoN,3);
-    PLo = zeros(bsn,ln); 
-    for l=1:ln
-        PLo(LoN(l),l) = Lcp(l);
-    end
-    Br_loss = zeros(brn,1);
-    for l=1:brn
-        if Br_o(l,14)>0 
-            Pb1(Br_o(l,1),Br_o(l,2)) = abs(Br_o(l,16));
-            Pb(Br_o(l,2),Br_o(l,1)) = Br_o(l,14);
-        else
-            Pb1(Br_o(l,2),Br_o(l,1)) = abs(Br_o(l,14));
-            Pb(Br_o(l,1),Br_o(l,2)) = Br_o(l,16);
+    function obj_hourly = CEF_hourly(mpc)
+        % result
+        [results, success] = runpf(mpc);
+        
+        if success == 0
+            obj_hourly = abs(sum(Pg) - sum(mpc.bus(:, 3)));
+            return
         end
-    %     Br_loss(l) = abs(Br_o(l,14)+Br_o(l,16));
-    end
-    % aaaa = [Pb1;PG'];
-    Pnd = diag(ones(1,bsn+gn)*[Pb1;PG']);
-    Pnd_p = Pnd;   Pb1_p = Pb1;  Rg_p = Rg;  Pb_p = Pb;   PLo_p = PLo;
-    if ~det(Pnd)
-        dl = find(sum(Pnd)==0);
-        Pnd_p(dl,:)=[];  Pnd_p(:,dl)=[];
-        Pb1_p(dl,:)=[];  Pb1_p(:,dl)=[];
-        Pb_p(dl,:)=[];   Pb_p(:,dl)=[];
-        Rg_p(dl)=[];     PLo_p(dl,:) = [];
-    end
-    En_p = (inv(Pnd_p-Pb1_p'))*Rg_p;       % ICEE intensity
-    %% ICEF rate  & BCEF rate  & BCEL rate
-    RL_p = diag(En_p)*PLo_p;          % ICEF rate
-    Rb1_p = diag(En_p)*Pb1_p;         % BCEF rate
-    % aaaa = (Pb_p'-Pb1_p);
-    Rl_p = diag(En_p)*(Pb_p'-Pb1_p);   % BCEL rate
-    %% 还原删减矩阵
-    dbl = zeros(1,ln);     dbu = zeros(1,bsn);
-    RL=RL_p;  Rb1=Rb1_p;  Rl=Rl_p; En=En_p;
     
-    if ~det(Pnd)
-        for d=1:length(dl)
-            RL = [RL(1:dl(d)-1,:);zeros(1,length(RL(1,:)));RL(dl(d):end,:)];
-            %     RL_m = [RL_m(:,1:dl(d)-1),zeros(length(RL_m(:,1)),1),RL_m(:,dl(d):end)];
-            Rb1 = [Rb1(1:dl(d)-1,:);zeros(1,length(Rb1(:,1)));Rb1(dl(d):end,:)];
-            Rb1 = [Rb1(:,1:dl(d)-1),zeros(length(Rb1(:,1)),1),Rb1(:,dl(d):end)];
-            Rl = [Rl(1:dl(d)-1,:);zeros(1,length(Rl(:,1)));Rl(dl(d):end,:)];
-            Rl = [Rl(:,1:dl(d)-1),zeros(length(Rl(:,1)),1),Rl(:,dl(d):end)];
-            En = [En(1:dl(d)-1);0;En(dl(d):end)];
+        %% 节点分类  
+        Ejn = results.gen(:,1);  gn = length(Ejn);
+        bsn = length(results.bus(:,1));  brn = length(results.branch(:,1));
+        Ijn = results.bus(:,1);
+        Ijn(Ejn)=[];
+        %% ECEF intensity 
+        PG = zeros(bsn,gn); 
+        for g=1:gn
+            PG(Ejn(g),g) = results.gen(g,2);
         end
+        Rg = PG*Eg;                % ECEF rate 
+        %% ICEE intensity
+        Br_o = results.branch;
+        Pb1 = zeros(bsn);   %节点注入功率矩阵
+        Pb = zeros(bsn);    %节点流出功率矩阵
+        LoN = find(results.bus(:,3)>0);   ln = length(LoN);   
+        Lcp = results.bus(LoN,3);
+        PLo = zeros(bsn,ln); 
+        for l=1:ln
+            PLo(LoN(l),l) = Lcp(l);
+        end
+        Br_loss = zeros(brn,1);
+        for l=1:brn
+            if Br_o(l,14)>0 
+                Pb1(Br_o(l,1),Br_o(l,2)) = abs(Br_o(l,16));
+                Pb(Br_o(l,2),Br_o(l,1)) = Br_o(l,14);
+            else
+                Pb1(Br_o(l,2),Br_o(l,1)) = abs(Br_o(l,14));
+                Pb(Br_o(l,1),Br_o(l,2)) = Br_o(l,16);
+            end
+        %     Br_loss(l) = abs(Br_o(l,14)+Br_o(l,16));
+        end
+        % aaaa = [Pb1;PG'];
+        Pnd = diag(ones(1,bsn+gn)*[Pb1;PG']);
+        Pnd_p = Pnd;   Pb1_p = Pb1;  Rg_p = Rg;  Pb_p = Pb;   PLo_p = PLo;
+        if ~det(Pnd)
+            dl = find(sum(Pnd)==0);
+            Pnd_p(dl,:)=[];  Pnd_p(:,dl)=[];
+            Pb1_p(dl,:)=[];  Pb1_p(:,dl)=[];
+            Pb_p(dl,:)=[];   Pb_p(:,dl)=[];
+            Rg_p(dl)=[];     PLo_p(dl,:) = [];
+        end
+        En_p = (inv(Pnd_p-Pb1_p'))*Rg_p;       % ICEE intensity
+        %% ICEF rate  & BCEF rate  & BCEL rate
+        RL_p = diag(En_p)*PLo_p;          % ICEF rate
+        Rb1_p = diag(En_p)*Pb1_p;         % BCEF rate
+        % aaaa = (Pb_p'-Pb1_p);
+        Rl_p = diag(En_p)*(Pb_p'-Pb1_p);   % BCEL rate
+        %% 还原删减矩阵
+        dbl = zeros(1,ln);     dbu = zeros(1,bsn);
+        RL=RL_p;  Rb1=Rb1_p;  Rl=Rl_p; En=En_p;
+        
+        if ~det(Pnd)
+            for d=1:length(dl)
+                RL = [RL(1:dl(d)-1,:);zeros(1,length(RL(1,:)));RL(dl(d):end,:)];
+                %     RL_m = [RL_m(:,1:dl(d)-1),zeros(length(RL_m(:,1)),1),RL_m(:,dl(d):end)];
+                Rb1 = [Rb1(1:dl(d)-1,:);zeros(1,length(Rb1(:,1)));Rb1(dl(d):end,:)];
+                Rb1 = [Rb1(:,1:dl(d)-1),zeros(length(Rb1(:,1)),1),Rb1(:,dl(d):end)];
+                Rl = [Rl(1:dl(d)-1,:);zeros(1,length(Rl(:,1)));Rl(dl(d):end,:)];
+                Rl = [Rl(:,1:dl(d)-1),zeros(length(Rl(:,1)),1),Rl(:,dl(d):end)];
+                En = [En(1:dl(d)-1);0;En(dl(d):end)];
+            end
+        end
+        % carbon emission rate 
+        CR_g = Eg.*results.gen(:,2);  % tCO2/h
+        CR_n = En.*results.bus(:,3);
+        CR_loss = sum(sum(Rl));       % =sum(CR_g)-sum(CR_n)
+        % carbon emission rate quantity
+        CE_Gen = CR_g*T;
+        CE_Load = CR_n*T;
+        CE_Loss = CR_loss*T;
+    
+        % target
+        obj_hourly = sum(Rb1(target_ind));
     end
-    % carbon emission rate 
-    CR_g = Eg.*results.gen(:,2);  % tCO2/h
-    CR_n = En.*results.bus(:,3);
-    CR_loss = sum(sum(Rl));       % =sum(CR_g)-sum(CR_n)
-    % carbon emission rate quantity
-    CE_Gen = CR_g*T;
-    CE_Load = CR_n*T;
-    CE_Loss = CR_loss*T;
 
-    % target
-    obj = sum(Rb1(target_ind));
 end
--- test.m
+++ test.m
@@ -6,39 +6,62 @@ define_constants;
 % a = 100;
 % b = 1; % define constant values
 % lb = [-3,-3];
-% ub = [3,3];
+% ub = [3, 3];
 % numberOfVariables = 2;
 % FitnessFunction = @(x) parameterized_fitness(x,a,b);
-% [x,fval] = ga(FitnessFunction,numberOfVariables,[],[],[],[],lb,ub);
+% [x,fval] = ga(FitnessFunction, numberOfVariables,[],[],[],[],lb,ub);
 
-
-T = 1;    % 持续时间 h
 mpc = loadcase('case39');
 n_bus = size(mpc.bus, 1);
-Gn = length(mpc.gen(:, 1));  %bsn = length(mpc.bus(:,1));   brn = length(mpc.branch(:,1));
+Gn = length(mpc.gen(:, 1));  %bsn = length(mpc.bus(:, 1));   brn = length(mpc.branch(:, 1));
 Eg = zeros(Gn, 1);           %  ECEF intensity   %发电机碳排放强度
 Eg(mpc.gen(:, 2) > 600) = 0.875;      % 单位：tCO2/M·Wh    
 Eg(300 < mpc.gen(:,2) & mpc.gen(:, 2) <= 600) = 0.500;  
 
-nvars = T * size(mpc.gen, 1); % T * 机组数量
-ObjectiveFunction = @(Pg) CEF_case39(T, mpc, Eg, Pg);
+loadProfile = load("data/Case39_LoadData_120Hours.mat");
+HourlyLoadProfile = mean(reshape(loadProfile.PD, [39, 24, 5]), 3);
+
+T = 24;
+nGens = size(mpc.gen, 1);
+nvars = T * nGens; % T * 机组数量
+ObjectiveFunction = @(Pg) CEF_case39(T, mpc, Eg, HourlyLoadProfile, Pg);
 % ConstraintFunction = @(Pg) para_costr(mpc, Pg, mpc.bus(:, PD));
 
 PTDF = makePTDF(mpc);
 
-A = PTDF(:, end - 9:end);
-b = PTDF * mpc.bus(:, PD) + mpc.branch(:, RATE_A);
-Aeq = ones(nvars, 1)';
-beq = sum(mpc.bus(:, PD));
+% 初始化A和b
+A = zeros(T * size(PTDF, 1), nvars);
+b = zeros(T * size(PTDF, 1), 1);
+
+% 构造A和b
+for t = 1:T
+    A((t-1)*size(PTDF, 1) + 1 : t*size(PTDF, 1), (t-1)*nGens + 1 : t*nGens) = PTDF(:, end - nGens + 1:end);
+    b((t-1)*size(PTDF, 1) + 1 : t*size(PTDF, 1)) = PTDF * HourlyLoadProfile(:, t) + mpc.branch(:, RATE_A);
+end
+
+% 初始化 Aeq 和 beq
+Aeq = zeros(T, nvars);
+beq = zeros(T, 1);
+
+% 构造 Aeq 和 beq
+for t = 1:T
+    Aeq(t, (t-1)*nGens + 1 : t*nGens) = ones(1, nGens);
+    beq(t) = sum(HourlyLoadProfile(:, t));
+end
 
 % lower bound & upper bound
-lb = mpc.gen(:, PMIN);
-ub = mpc.gen(:, PMAX);
+% lb = repmat(mpc.gen(:, PMIN), T, 1);
+% ub = repmat(mpc.gen(:, PMAX), T, 1);
+
+lb = [];
+ub = [];
 
 meanPg = sum(mpc.bus(:, 3)) / size(mpc.gen, 1);
 rng default % For reproducibility
 options = optimoptions("ga",'PlotFcn', {@gaplotbestf,@gaplotmaxconstr}, 'Display', 'iter');
-options.InitialPopulationMatrix = ones(size(mpc.gen(:, 2)))' * sum(mpc.bus(:, 3)) / size(mpc.gen, 1);
+
+% 初始设置每个发电机组 500
+options.InitialPopulationMatrix = repmat(ones(size(mpc.gen(:, 2))) * 500, T, 1)';
 options.PopulationSize = 50;
 [x, fval] = ga(ObjectiveFunction, nvars, A, b, Aeq, beq, lb, ub, [], options)
 
